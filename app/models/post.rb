879ab1fdd9287b801a17d8822b14f8318bb4ee37
Brian <wamwangimathenge@gmail.com>
2023-03-15 10:44:40 +0300

    Inclusion syntax modified

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
     validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
-    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates :category, inclusion: [ "Fiction", "Non-Fiction" ]
 end
