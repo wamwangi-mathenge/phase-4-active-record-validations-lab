9c9e0a99fd55a3d1102c6a61b2732e3f3051fcff
Brian <wamwangimathenge@gmail.com>
2023-03-16 16:09:59 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: [ "Fiction", "Non-Fiction" ]
+    validate :clickbaity_title
+
+    def clickbaity_title
+        unless title.present? && title.match?(/(secret)|(won't believe)|(guess)|(top [0-9]+)/i)
+            errors.add(:title, "Title not clickbait-y")
+        end
+    end
+    
 end
